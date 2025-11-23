c523d487d6dd8da0260a256eac5670c25983a966
asissuban <ignasiusbolisuban@gmail.com>
2025-11-23 17:27:42 +0700

    membuat website kopi senjakoe

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle class (yang tadinya ada jadi ngak ada, dan yang tadinya ngak ada jadi ada)
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
